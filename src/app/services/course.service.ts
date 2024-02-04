f40867be387d93879a023696474be8af1592222f
Matheus Rocha <im.rochamatheus@gmail.com>
2024-02-03 22:17:09 -0300

    refactor: impedir que sejam realizadas novas requisições independentemente do número de assinaturas

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, shareReplay } from "rxjs/operators";
 
 import { Course } from "../model/course";
 
@@ -13,8 +13,9 @@ export class CourseService {
   constructor(private readonly http: HttpClient) {}
 
   public loadAllCourses(): Observable<Course[]> {
-    return this.http
-      .get<Course[]>("/api/courses")
-      .pipe(map((res) => res["payload"]));
+    return this.http.get<Course[]>("/api/courses").pipe(
+      map((res) => res["payload"]),
+      shareReplay()
+    );
   }
 }
